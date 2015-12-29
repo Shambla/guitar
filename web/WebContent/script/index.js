1b1d632be8a3fa414dba5954a771df278aa71a09
Bill Streckfus <streckfus_william@bah.com>
2015-12-29 01:29:57 -0500

    Fleshing out other sections, added youtube API

--- web/WebContent/script/index.js
+++ web/WebContent/script/index.js
@@ -1,6 +1,22 @@
+var apiKey = "AIzaSyCNdFVx45bBA3_402O-UUREh9MmFL0vS0I";
+var channelId = "UCBirLir8z4yrq3HEV5idxaA";
 
 $(document).ready(function() {
 	setTimeout(function() {$("#performer").addClass("active");}, 500);
 	setTimeout(function() {$("#composer").addClass("active");}, 1200);
 	setTimeout(function() {$("#teacher").addClass("active");}, 2000);
+	
+	randomizeBars();
+	
+	gapi.client.setApiKey(apiKey);
 });
+
+function randomizeBars() {
+	$(".bar").each(function(i) {
+		var boost = (i === 1 || i === 3 ? 2 : (i === 2 ?  4 : 0));
+		var height = 5 + boost + Math.random() * (15 - boost) - (4 - boost);
+		$(this).css("height", height + "px").css("margin-top", 20 - height + "px");
+	});
+	
+	setTimeout(randomizeBars, 200);
+}
