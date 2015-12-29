c2f360a65c3de576e24e663f9f8ba55a054ce03a
Bill Streckfus <streckfus_william@bah.com>
2015-12-29 15:50:14 -0500

    Music player

--- web/WebContent/script/index.js
+++ web/WebContent/script/index.js
@@ -1,12 +1,35 @@
 var apiKey = "AIzaSyCNdFVx45bBA3_402O-UUREh9MmFL0vS0I";
 var channelId = "UCBirLir8z4yrq3HEV5idxaA";
 
+var res;
+
+var heights = {};
+
 $(document).ready(function() {
 	setTimeout(function() {$("#performer").addClass("active");}, 500);
 	setTimeout(function() {$("#composer").addClass("active");}, 1200);
 	setTimeout(function() {$("#teacher").addClass("active");}, 2000);
 	
 	randomizeBars();
+	
+	heights.bio = $("#bio").offset().top;
+	
+	window.addEventListener("scroll", scrolled);
+	
+	$(function() {
+		  $('a[href*=#]:not([href=#])').click(function() {
+		    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+		      var target = $(this.hash);
+		      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+		      if (target.length) {
+		        $('html,body').animate({
+		          scrollTop: target.offset().top
+		        }, 1000);
+		        return false;
+		      }
+		    }
+		  });
+		});
 });
 
 function randomizeBars() {
@@ -23,6 +46,27 @@ function loadYoutube() {
 	gapi.client.setApiKey(apiKey);
 	
 	gapi.client.load('youtube', 'v3', function() {
-		console.log("loaded");
+		var request = gapi.client.youtube.channelSections.list({
+			channelId: channelId,
+			part: "contentDetails"
+		});
+		
+		request.execute(function(response) {
+			res = response;
+		});
 	});
+}
+
+function scrolled() {
+	var distance = window.pageYOffset || document.documentElement.scrollTop;
+	
+	if(distance > 20) {
+		$("#backing").addClass("scrolled");
+	} else {
+		$("#backing").removeClass("scrolled");
+	}
+	
+	var heroOpacity = 1 - ((distance - heights.bio / 2.2) / (heights.bio / 2.2));
+	
+	$("#hero").css("opacity", Math.max(0, Math.min(1, heroOpacity)));
 }
