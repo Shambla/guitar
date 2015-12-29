d73b1c40bad5737c5e1e05bd1f8ad464b772bea4
Bill Streckfus <streckfus_william@bah.com>
2015-12-28 23:20:46 -0500

    Fonts, background, hero setup

---
+++ web/WebContent/script/index.js
@@ -0,0 +1,6 @@
+
+$(document).ready(function() {
+	setTimeout(function() {$("#performer").addClass("active");}, 500);
+	setTimeout(function() {$("#composer").addClass("active");}, 1200);
+	setTimeout(function() {$("#teacher").addClass("active");}, 2000);
+});
